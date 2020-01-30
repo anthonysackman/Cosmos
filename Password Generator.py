a614764679631f5af3f84120deb3dcbf355ba422
anthonysackman <60375685+anthonysackman@users.noreply.github.com>
2020-01-29 21:02:12 -0800

    update

--- Password Generator.py
+++ Password Generator.py
@@ -8,13 +8,13 @@ while live:
     user_input1 = int(input("How Long Do You Want Your Password?"))
     user_input2 = int(input("How Many Letters?"))
     while user_input2 > user_input1:
-        error_input1 = int(input("Number Higher Than Total Password Legnth, Please Try Again."))
+        error_input1 = int(input("Number Higher Than Total Password Length, Please Try Again."))
         user_input2 = error_input1
         continue
     if user_input1 != user_input2:
         user_input3 = int(input("How Many Numbers?"))
         while user_input1 < user_input2 + user_input3:
-            error_input3 = int(input("Combined Number Higher Than Total Password Legnth, Please Try Again."))
+            error_input3 = int(input("Combined Number Higher Than Total Password Length, Please Try Again."))
             user_input3 = error_input2
             continue
         if user_input1 != user_input2 + user_input3:
